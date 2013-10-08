a306d55ec81a3f5b6cf8c4aee2e0e340e0f6e871
Harold Pimentel <haroldpimentel@gmail.com>
2013-10-07 19:31:10 -0700

    Removed a bunch of old functions. Did some cleaning of driver

--- biclustering.R
+++ biclustering.R
@@ -1,131 +1,8 @@
 # Author: Harold Pimentel
 # Contact: pimentel@cs.berkeley.edu
 
-# Provides 'ipop()' for quadratic programming
-library(kernlab)
 # Lee et. al. package. Provides 'scca()'
 library(scca)
-# Provides 'quadprog()'
-library(quadprog)
-
-##' One iteration of biclustering maximization. Takes the current values of 
-##' d, a, b and if successful returns one iteration
-##' 
-##' @param xDf matrix with rows representing genes and columns representing conditions (n x k)
-##' @param yDf matrix with rows representing genes and columns representing conditions (n x k)
-##' @param d vector of dimension k
-##' @param a vector of dimension n
-##' @param b vector of dimension n
-##' @return A list containing maximum values of a, b, d
-biClustMax <- function(xDf, yDf, d, a, b, maxIt = 5, verbose = TRUE)
-{
-    quadRes <- 0
-    quadIt <- 0
-
-    # iterate until we've hit maximum iterations or QP has converged
-    while (quadIt < maxIt & class(quadRes) == "numeric")
-    {
-        quadIt <- quadIt + 1
-        if (verbose)
-            cat("Computing tilde matrices\n")
-        xTilde <- t(xDf %*% diag(d))
-        yTilde <- t(yDf %*% diag(d))
-
-        if (verbose)
-            cat("Computing SCCA component\n")
-        # First maximize a and b using SCCA
-        sccaRes <- scca(as.matrix(xTilde), as.matrix(yTilde), 
-                        nc = 1, 
-                        penalty = "HL",
-                        center = TRUE, scale = FALSE)
-        a <- as.numeric(sccaRes$A)
-        b <- as.numeric(sccaRes$B)
-
-        # Now, maximize D using quadratic programming
-        #
-        # FIXME: Q ends up being singular. Way to solve is by making d sparse
-        Q <- - 2 * diag(as.numeric((a %*% xDf) * (b %*% yDf)))
-        return(Q)
-        if (verbose)
-        {
-            cat("Solving quadratic program\n")
-        }
-
-        exitStatus <- tryCatch(
-                               quadRes <- ipop(c = rep(0, length(d)),
-                                               H = Q,
-                                               A = diag(length(d)),
-                                               b = rep(0, length(d)),
-                                               l = rep(0, length(d)),
-                                               u = rep(1, length(d)),
-                                               r = rep(1, length(d))
-                                               ),
-                               error = function(e) {
-                                   message(paste("ipop message:\n\t", e))
-                                   return(1)
-                               },
-                               finally = {
-                                   message(paste("Quadratic program completed iteration", quadIt, "/", maxIt))
-                               })
-    }
-
-    if (exitStatus)
-        stop("Non-zero exit status from 'kernlab.' Results from SCCA make 'H' matrix singular (see 'ipop' call).")
-    if (verbose)
-        cat("Successfully found solutation to QP\n")
-
-    return(list(a = a, b = b, d = quadRes@primal))
-}
-
-
-##' One iteration of biclustering maximization. Takes the current values of 
-##' d, a, b and if successful returns one iteration
-##' 
-##' @param xDf matrix with rows representing genes and columns representing conditions (n x k)
-##' @param yDf matrix with rows representing genes and columns representing conditions (n x k)
-##' @param d vector of dimension k
-##' @param a vector of dimension n
-##' @param b vector of dimension n
-##' @return A list containing maximum values of a, b, d
-biClustMax.nlminb <- function(xDf, yDf, d, a, b, maxIt = 5, verbose = TRUE)
-{
-
-    # iterate until we've hit maximum iterations or QP has converged
-    if (verbose)
-        cat("Computing tilde matrices\n")
-    xTilde <- t(xDf %*% diag(d))
-    yTilde <- t(yDf %*% diag(d))
-
-    if (verbose)
-        cat("Computing SCCA component\n")
-    # First maximize a and b using SCCA
-    sccaRes <- scca(as.matrix(xTilde), as.matrix(yTilde), 
-                    nc = 1, 
-                    penalty = "HL",
-                    center = TRUE, scale = FALSE)
-    a <- as.numeric(sccaRes$A)
-    b <- as.numeric(sccaRes$B)
-
-    # Now, maximize D using quadratic programming
-    q <- - 2 * as.numeric((a %*% xDf) * (b %*% yDf))
-
-    if (verbose)
-        cat("Solving optimization program\n")
-
-    objectiveQ <- function(d, lam = 0)
-    {
-        sum(0.5 * d^2 * q - lam * sum(abs(d)))
-    }
-
-    startVec <- runif(length(q))
-    optimRes <- nlminb(startVec, objectiveQ, 
-                       lower = rep.int(0, length(startVec)), 
-                       upper = rep.int(1, length(startVec)),
-                       lam = 0)
-
-    return(list(a = a, b = b, d = optimRes, q))
-}
-
 
 ##' One iteration of biclustering maximization. Takes the current values of 
 ##' d, a, b and if successful returns one iteration
@@ -183,7 +60,7 @@ biClustMax.optim <- function(xDf, yDf, d.start, a, b, lam, verbose = TRUE)
 
     if (optimRes$convergence != 0)
     {
-        warning("Error with convergence. Check\n", "\tError code: ", 
+        warning("Error with convergence.\n", "\tError code: ", 
                 optimRes$convergence, "\tMessage: ", optimRes$message)
     }
 
@@ -191,111 +68,8 @@ biClustMax.optim <- function(xDf, yDf, d.start, a, b, lam, verbose = TRUE)
 }
 
 
-quadraticMax <- function(QMat, c, bound)
-{
-    dDim <- dim(QMat)[1]
-    Amat <- matrix(0, nrow = dDim, ncol = dDim)
-    sol <- ipop(c = rep(c, dDim),
-                H = QMat,
-                A = Amat,
-                b = rep(0, dDim),
-                l = rep(0, dDim),
-                u = rep(1000, dDim),
-                r = rep(0, dDim),
-                bound = bound
-                #margin = 0.05
-                )
-
-    return(sol)
-}
-
-
-# same as quadraticMax, but puts constraints on cluster size.
-quadraticMax.clusterSize <- function(QMat, c, bound, clusterSize)
-{
-    dDim <- dim(QMat)[1]
-    constraintMat <- matrix(0, nrow = dDim, ncol = dDim)
-    constraintMat[1,] <- 1
-    sol <- ipop(c = rep(c, dDim),
-                H = QMat,
-                A = constraintMat,
-                b = rep(0, dDim),
-                r = c(clusterSize, rep(0, dDim - 1)),
-                l = rep(0, dDim),
-                u = rep(1, dDim),
-                bound = bound
-                )
-    return(sol)
-}
-
-
-quadProgMax <- function(DMat, lam)
-{
-    dDim <- dim(DMat)[1]
-    # constraints that values must be greater than zero
-    AMat <- diag(dDim)
-    b0 <- rep(0, dDim)
-    # constraints that values must be less than 1
-    AMat <- rbind(AMat, -diag(dDim))
-    b0 <- c(b0, rep(-1, dDim))
-    # constraint that the sum of them are less than lam (regularization)
-    # AMat <- rbind(AMat, rep(1, dDim))
-    # b0 <- c(b0, lam)
-    # dimensions are weird...
-    AMat <- t(AMat)
-
-    sol <- solve.QP(DMat, rep(lam, dDim), AMat, b0)
-
-    return(sol)
-}
-
-library(QP)
-
-cgalMax <- function(DMat, lam)
-{
-    dDim <- dim(DMat)[1]
-    AMat <- matrix(0, nrow = 1, ncol = dDim)
-    # AMat[1,] <- 1
-    #sol <- cgalC(DMat, AMat, c(lam, rep(0, dDim-1)), 
-    sol <- cgalQP(DMat, AMat, c(0), 
-          rep(0, dDim), rep(1, dDim),
-          rep(lam, dDim), 0)
-
-    return(sol)
-}
-
-debug(quadProgMax)
-undebug(quadProgMax)
-
-quadProgMax(diag(x = 4, 3), 5)
-
-quadProgMax(meow2, 5)
-
-cgalMax(meow2, 1)
-
-cgalMax(diag(5), 1)
-
-
-solve.QP(diag(3), 1:3, matrix(0, 3, 3), rep(0, 3))
-
-hi <- quadraticMax(meow, 0, 10)
-
-hi <- quadraticMax(diag(5), 0, 10)
-
-hi <- cgalMax(diag(5), 3)
-
-hi <- quadraticMax(meow, -100000, 0.5)
-
-hi <- quadraticMax(meow2, 0, 10)
-hi <- quadraticMax(meow, -100000, 0.5)
-
-quadraticMax(meow, 10000000, 100)
-quadraticMax(meow, abs(0.0001*sum(meow)), 100)
-
-sol <- quadraticMax.clusterSize(meow, 0, 10, max(abs(meow)))
-
-debug(biClustMax)
-undebug(biClustMax)
+debug(biClustMax.optim)
+undebug(biClustMax.optim)
 
 debug(NIPALS.sparse)
 undebug(NIPALS.sparse)
@@ -305,6 +79,7 @@ undebug(opt.cv.alt)
 
 debug(scca)
 undebug(scca)
+
 
 ##' Randomly splits indices if there are an even number
 ##' 
@@ -321,8 +96,6 @@ splitEvenly <- function(n)
 }
 
 
-biClustMax(X, Y, diag(Dx), 1, 1)
-
 ##' Driver function to find solution for 
 maximizeOneSplit <- function(geneDf)
 {
@@ -330,9 +103,24 @@ maximizeOneSplit <- function(geneDf)
     splitIdx <- splitEvenly(nrow(geneDf))
     xDf <- geneDf[splitIdx[[1]], ]
     yDf <- geneDf[splitIdx[[2]], ]
-    d <- runif(ncol(xDf))
 
-    biClustMax(xDf, yDf, d, 1, 1)
+    lam <- round(min(xDf) * .3)
+
+    d <- 0
+    repeat {
+        d <- runif(ncol(xDf))
+        if (sum(d) <= lam)
+            break
+    }
+
+    # NB: values of a and b are not important currently since evaluated after
+    aVal <- list()
+    bVal <- list()
+    dVal <- list()
+    repeat {
+        biClustMax(xDf, yDf, d, 1, 1, lam)
+
+    }
 }
 
 
